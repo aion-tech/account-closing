1e61bda68a6d2ad79fbcaf306f8228215bf59999
Luca Franceschini <lucafranceschini@LukeBook.local>
2024-02-23 12:53:02 +0100

    [IMP] marco_importer: product import

--- marco_importer/wizard/importer.py
+++ marco_importer/wizard/importer.py
@@ -4,19 +4,20 @@ import requests
 from odoo import api, fields, models
 
 
+
 class MarcoImporter(models.TransientModel):
     _name = "marco.importer"
 
     url = fields.Char(
         string="URL",
         required=True,
-        default="https://api.marco.it/odoo/clients",
+        default="https://api.marco.it/odoo/items",
     )
     import_type = fields.Selection(
-        selection=[("partner", "Partner"), ("bom", "BOM")],
+        selection=[("partner", "Partner"), ("bom", "BOM"),("items","Items")],
         string="Import Type",
         required=True,
-        default="partner",
+        default="items",
     )
 
     def _check_url(self):
@@ -28,10 +29,58 @@ class MarcoImporter(models.TransientModel):
 
         res = requests.get(self.url)
         records = res.json()
-        if self.report_type == "partner":
+        if self.import_type == "items":
+            self.import_items(records)
+
+        if self.import_type == "partner":
             self.import_partner_data(records)
-        if self.report_type == "bom":
+
+        if self.import_type == "bom":
             self.import_bom_data(records)
+
+    def import_items(self,records):
+        for rec in records[:10]:
+            #uom=self.env.ref(rec["uom_id"])
+            uom=self.env["uom.uom"].search([("name","=",rec["uom_id"])])
+            uom_po=self.env["uom.uom"].search([("name","=",rec["uom_po_id"])])
+            vals = {
+                "default_code": rec["default_code"],
+                "name": rec["name"],
+                "barcode": rec["barcode"],
+                "sale_ok": rec["sale_ok"]=="1",
+                "purchase_ok": rec["purchase_ok"]=="1",
+                "uom_id": uom.id,
+                "uom_po_id": uom_po.id,
+                "detailed_type": rec["detailed_type"],
+                "standard_price": rec["standard_price"],
+                "list_price":rec["basePrice"]
+            }
+            
+           
+            existing_item = self.env["product.template"].search(
+                [("default_code", "=", rec["default_code"])]
+
+            )
+            if existing_item:
+                existing_item.write(vals)
+                #print(uom,uom_po,vals)
+            else:
+                existing_item = self.env["product.template"].create(vals)
+                #print(uom,uom_po,vals)
+            existing_item_product=self.env["product.product"].search([("product_tmpl_id","=",existing_item.id)])
+
+            update_qty_wizard=self.env["stock.change.product.qty"].new({"product_tmpl_id":existing_item.id,"product_id":existing_item_product.id,"new_quantity":12})
+            update_qty_wizard.change_product_qty()
+
+            stock_quant_id=self.env["stock.quant"].search([("quantity",">=",0),("product_id","=",existing_item_product.id)])
+            if stock_quant_id:
+                stock_quant_id.action_apply_inventory()
+                print(stock_quant_id)
+           
+
+            
+            #self.env.cr.commit()
+        print("import terminato")
 
     def import_bom_data(self, records):
         # product = self.env["product.template"].create(
@@ -85,4 +134,4 @@ class MarcoImporter(models.TransientModel):
             existing_partner.email = False
 
             print(existing_partner.name)
-            self.env.cr.commit()
+            #self.env.cr.commit()
