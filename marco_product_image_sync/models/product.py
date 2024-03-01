7f0c6397b20e622d79cc493b51aaeb7eb39a004f
Luca Franceschini <lucafranceschini@LukeBook.local>
2024-03-01 17:05:52 +0100

    [IMP] marco_product_image_sync: prima versione funzionante, prendo solo prodotti che posso vendere.

--- marco_product_image_sync/models/product.py
+++ marco_product_image_sync/models/product.py
@@ -1,8 +1,9 @@
 from typing import List
 
 import requests
-from odoo import Command, api, fields, models
-
+from odoo import Command, api, fields, models, Command
+import logging
+_logger=logging.getLogger(__name__)
 URL = "https://api.marco.it/odoo/images"
 
 
@@ -11,16 +12,40 @@ class ProductTemplate(models.Model):
 
     @api.model
     def product_image_sync(self):
-        product_ids = self.search([])
-        for product in product_ids:
-            images: List[str] = requests.get(
+        product_ids = self.search([("sale_ok","=",1)],order="default_code asc")
+        for idx,product in enumerate(product_ids):
+            res: List[str] = requests.get(
                 URL, params={"default_code": product.default_code}
-            ).json()
+            )
+            _logger.error(f'{product.default_code},{res.status_code}')
+            if res.status_code==404:
+                continue
+            images=res.json()
             for image in images:
-                product.image_1920 = image
+                #__import__('pdb').set_trace()
+                if image["name"]=="Primary":
+                    product.image_1920 = image["image"]
+                #[{"image":"image1","name":"primary"}]
+                    
+                existingImage=product.product_template_image_ids.filtered(lambda img: img.name==image["name"])
+                if existingImage:
+                    if image["image"]!=existingImage.image_1920:
+                        existingImage.write({
+                            "image_1920":image["image"]
+                            })
+                else:
+                    existingImage.create({
+                        "name":image["name"],
+                        "image_1920":image["image"]
+                    })
+            #_logger.warning(product.name,idx,len(product_ids))
+
+            """  
                 product.product_template_image_ids = [
                     (2, old_img.id, 0) for old_img in product.product_template_image_ids
                 ]
+               
                 product.product_template_image_ids = [
                     (0, 0, {"image_1920": image, "name": "immagine123"})
-                ]
+                ] 
+            """
