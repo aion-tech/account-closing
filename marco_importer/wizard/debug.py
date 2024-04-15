66ad4630d017d15763271bdf0dcd087b5f159c44
Luca Franceschini <luca.franceschini@marco.it>
2024-04-15 11:24:48 +0200

    [STYLE] marco_importer: aggiunta l'icona creata con figma partendo da un template trovato sull'INTERNET

--- marco_importer/wizard/debug.py
+++ marco_importer/wizard/debug.py
@@ -14,7 +14,7 @@ class MarcoImporter(models.TransientModel):
         bom_line = self.env["mrp.bom.line"].search([("bom_id", "=", bom.id)])
         __import__("pdb").set_trace()
         """
-
+        # con new lo lasci in memoria, con create invece lo crei nel database e viene pulito una volta al giorno  dal crono "auto vacuum ..."
         self.env["res.config.settings"].new({"group_uom": True}).execute()
         self.env["res.config.settings"].new({ "group_product_variant": True}).execute()
         self.env["res.config.settings"].new({ "group_stock_multi_locations": True}).execute()
@@ -23,7 +23,7 @@ class MarcoImporter(models.TransientModel):
         self.env["res.config.settings"].new({"module_mrp_workorder":True}).execute()
         self.env["res.config.settings"].new({ "module_mrp_subcontracting":True}).execute()
 
-        # con new lo lasci in memoria, con create invece lo crei nel database e viene pulito una volta al giorno  dal crono "auto vacuum ..."
+        
         """
         # __import__("pdb").set_trace()
         res = self.env["stock.route"].search([])
