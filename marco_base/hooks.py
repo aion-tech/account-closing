2c3f7de2e933693c64ba31fd956fc3f9dd7b07f0
Luca Franceschini <luca.franceschini@marco.it>
2024-04-15 11:23:35 +0200

    [IMP] marco_base: creato l'hook che setta i flag all'interno delle impostazioni di odoo

--- marco_base/hooks.py
+++ marco_base/hooks.py
@@ -1,4 +1,11 @@
-
+from odoo import api, SUPERUSER_ID
 
 def marco_post_init_hook(cr,registry):
-    print("lippa")
+    env=api.Environment(cr,SUPERUSER_ID,{})
+    env["res.config.settings"].new({"group_uom": True}).execute()
+    env["res.config.settings"].new({ "group_product_variant": True}).execute()
+    env["res.config.settings"].new({ "group_stock_multi_locations": True}).execute()
+    env["res.config.settings"].new({ "group_stock_adv_location": True}).execute()
+    env["res.config.settings"].new({"group_mrp_routings":True}).execute()
+    env["res.config.settings"].new({"module_mrp_workorder":True}).execute()
+    env["res.config.settings"].new({ "module_mrp_subcontracting":True}).execute()
