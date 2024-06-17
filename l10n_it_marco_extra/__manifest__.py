ef4c7b3d671f8a3457e06e03adbc728d8125aac3
Luca Franceschini <luca.franceschini@marco.it>
2024-06-17 13:18:19 +0200

    [ADD] l10n_it_marco_extra: creato un modulo aggiuntivo con l'idea di creare dei registri dopo il caricamento del piano dei conti, perchè non si può fare contestualmente alla creazione dei conti perchè non abbiamo a disposizione il ref

---
+++ l10n_it_marco_extra/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Italy - Accounting - MARCO S.p.A.',
+    'version': '0.6',
+    'depends': [
+        'account',
+        'base_iban',
+        'base_vat',
+        'l10n_it_marco'
+    ],
+    'author': 'OpenERP Italian Community, MARCO S.p.A.',
+    'description': """
+Piano dei conti italiano della MARCO S.p.A. .
+================================================
+
+Italian accounting chart and localization.
+    """,
+    'category': 'Accounting/Localizations/Account Charts',
+    'website': 'http://www.odoo.com/',
+    'data': [
+       
+        'data/account_journal.xml',
+        
+    ],
+    'demo': [
+       # 'demo/demo_company.xml',
+    ],
+    'license': 'LGPL-3',
+}
