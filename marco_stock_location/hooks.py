2b03c00176a1fcc95a623965c8097031db62595e
Luca Franceschini <luca.franceschini@marco.it>
2024-08-28 11:47:55 +0200

    [IMP] marco_stock_location: aggiunta la nomenclatura custom gs1 per le location e aggiunto nell'hook la sua attivazione nelle impostazioni
    Signed-off-by: Luca Franceschini <luca.franceschini@marco.it>

--- marco_stock_location/hooks.py
+++ marco_stock_location/hooks.py
@@ -8,3 +8,8 @@ _logger = logging.getLogger(__name__)
 def marco_post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     env["res.config.settings"].new({"group_stock_multi_locations":True}).execute()
+    
+    config = env['res.config.settings'].create({})
+    # Imposta il valore di barcode_nomenclature_id
+    config.barcode_nomenclature_id = env.ref('marco_stock_location.marco_gs1_nomenclature')
+    config.execute()
