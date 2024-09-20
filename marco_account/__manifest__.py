62c8831167cdc194959165c05c222c600a2292f3
techgrrow <luca.magnani@aion-tech.it>
2024-09-20 15:07:41 +0200

    [ADD] marco_account, field/button on riba tree view

---
+++ marco_account/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "marco_account",
+    "summary": "Customizations regarding accounting",
+    "description": "Customizations regarding accounting",
+    "author": "Aion Tech Srl",
+    "website": "https://aion-tech.it/",
+    "category": "Uncategorized",
+    "version": "16.0.1.0.1",
+    "depends": [
+        "account",
+        "l10n_it_riba",
+    ],
+    "data": [
+        "views/riba.xml",
+    ],
+}
