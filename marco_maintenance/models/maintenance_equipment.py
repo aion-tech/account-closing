17d25d6375353881bffaf1726d751572b9ce98e5
gabriele-baldessari <gabriele.baldessari@gmail.com>
2024-09-25 14:09:37 +0200

    [IMP] marco_maintenance: maintenance request templates

--- marco_maintenance/models/maintenance_equipment.py
+++ marco_maintenance/models/maintenance_equipment.py
@@ -1,5 +1,4 @@
 import uuid
-from datetime import timedelta
 
 from odoo import api, fields, models
 
@@ -22,11 +21,13 @@ class MaintenanceEquipment(models.Model):
     category_id = fields.Many2one(
         required=True,
     )
-
-    delta_creation_date = fields.Integer(
-        string="Delta Creation Date",
-        default=60,
-        required=True,
+    maintenance_request_template_ids = fields.Many2many(
+        string="Templates",
+        comodel_name="maintenance.request.template",
+        relation="equipment_id_template_id_rel",
+        column1="equipment_id",
+        column2="template_id",
+        domain="[('category_id', '=', category_id)]",
     )
 
     @api.model
@@ -35,27 +36,51 @@ class MaintenanceEquipment(models.Model):
         Generates maintenance request on the
         next_action_date or today if none exists
         """
-        date_now = fields.Date.context_today(self)
+        equipments = self.search([("maintenance_request_template_ids", "!=", False)])
 
-        for equipment in self.search([("period", ">", 0)]):
-            # Calculate the check date as
-            # next_action_date - delta_creation_date
-            check_date = equipment.next_action_date - timedelta(
-                days=equipment.delta_creation_date
-            )
+        for equipment in equipments:
+            for tmpl in equipment.maintenance_request_template_ids:
+                if tmpl.end_date and tmpl.end_date < fields.Date.today():
+                    continue
 
-            # Check if today's date is greater than or equal to check_date
-            if date_now >= check_date or not equipment.delta_creation_date:
-                next_requests = self.env["maintenance.request"].search(
+                existing_request = self.env["maintenance.request"].search(
                     [
-                        ("stage_id.done", "=", False),
+                        ("template_id", "=", tmpl.id),
                         ("equipment_id", "=", equipment.id),
-                        ("maintenance_type", "=", "preventive"),
-                        ("request_date", "=", equipment.next_action_date),
-                    ]
+                    ],
+                    limit=1,
+                    order="request_date desc",
                 )
-                if not next_requests:
-                    equipment._create_new_request(equipment.next_action_date)
+
+                delta = tmpl._compute_period_in_days(
+                    tmpl.period_type,
+                    tmpl.period,
+                )
+                if (
+                    not existing_request
+                    or tmpl.start_date > existing_request.request_date
+                ):
+                    start_date = tmpl.start_date
+                else:
+                    start_date = existing_request.request_date
+
+                next_date = start_date + delta
+                no_open_requests = (
+                    existing_request.stage_id.done if existing_request else True
+                )
+                delta_creation = tmpl._compute_period_in_days(
+                    tmpl.delta_creation_date_period_type,
+                    tmpl.delta_creation_date,
+                )
+                if (
+                    next_date
+                    and next_date - delta_creation <= fields.Date.today()
+                    and no_open_requests
+                ):
+                    vals = tmpl._prepare_maintenance_request_vals(
+                        next_date, equipment.id
+                    )
+                    self.env["maintenance.request"].create(vals)
 
     def _get_document_folder(self):
         """
@@ -92,6 +117,9 @@ class MaintenanceEquipment(models.Model):
                 )
                 if folder_id:
                     folder_id.name = vals["serial_no"]
+
+            equipment.maintenance_request_template_ids._check_category_id_coherence()
+
         return res
 
     @api.model_create_multi
