a5ac088f7e9b3c402a01280c7f58228b86d00953
techgrrow <luca.magnani@aion-tech.it>
2024-11-14 10:59:02 +0100

    [ADD] marco_sale, marco account
    'campi da verificare sempre' group in form

---
+++ marco_sale/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "marco_sale",
+    "summary": "Customizations regarding sales",
+    "description": "Customizations regarding sales",
+    "author": "Aion Tech Srl",
+    "website": "https://aion-tech.it/",
+    "category": "Uncategorized",
+    "version": "16.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order.xml",
+    ],
+}
