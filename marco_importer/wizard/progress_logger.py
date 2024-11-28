bc71647ad5a17bfbad777e844117147a291326f1
Luca Franceschini <luca.franceschini@marco.it>
2024-11-28 11:20:12 +0100

    [IMP] marco_importer: migliorati i crono che sincronizzano gli ordini, migliorato il progress_logger
    Signed-off-by: Luca Franceschini <luca.franceschini@marco.it>

--- marco_importer/wizard/progress_logger.py
+++ marco_importer/wizard/progress_logger.py
@@ -1,10 +1,12 @@
 import logging
-
+import inspect
 
 _logger = logging.getLogger(__name__)
 
-
 def _progress_logger(iterator: int, all_records: list, additional_info: str = ""):
+    # Ottieni il nome della funzione chiamante
+    caller_function = inspect.stack()[1].function
+
     _logger.warning(
-        f"<--- {str(iterator+1)} | {str(len(all_records))} ---> {additional_info}"
+        f"Called from: {caller_function} | <--- {str(iterator+1)} | {str(len(all_records))} ---> {additional_info}"
     )
