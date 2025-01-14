f6bb0e5327791b7a8908a789b1906960d6245a46
Guidomadormo <68272608+Guidomadormo@users.noreply.github.com>
2025-01-14 15:26:59 +0100

    [FIX] can't write on invisible field journal_id

--- account_cutoff_start_end_dates/__manifest__.py
+++ account_cutoff_start_end_dates/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Account Cut-off Start End Dates",
-    "version": "16.0.1.1.4",
+    "version": "16.0.1.1.5",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "Cutoffs based on start/end dates",
